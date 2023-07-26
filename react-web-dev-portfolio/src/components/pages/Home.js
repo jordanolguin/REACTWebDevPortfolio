2d795704c0a5bc0654c9f9e1d930e6dfa2848a6c
Jordan Olguin <jordanolguin@iMac.local>
2023-07-26 10:14:39 -0600

    added typing effect

--- react-web-dev-portfolio/src/components/pages/Home.js
+++ react-web-dev-portfolio/src/components/pages/Home.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="neonBorderHome">
       <h1>jordan olguin</h1>
-      <h2>full stack web dev and yogi</h2>
+      <h2 className="typed">full stack web dev and yogi</h2>
     </div>
   );
 }
